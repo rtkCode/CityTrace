64f7c56f93bea12ba2502235c9b439185b4bc683
Tianze Wen <tianze.wen@ucdconnect.ie>
2023-01-11 22:38:20 +0000

    fix uppercase attribute

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -39,7 +39,7 @@ export function MarkerIcon(props) {
 
     return(
         <div className="icon" style={markerIconStyle} onClick={props.onClick}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={props.isMarkerShown ? "#ffffff" : "#000000"} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="10" r="3"/><path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={props.isMarkerShown ? "#ffffff" : "#000000"} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="10" r="3"/><path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z"/></svg>
         </div>
     )
 }
